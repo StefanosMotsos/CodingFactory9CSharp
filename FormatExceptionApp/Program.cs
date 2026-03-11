2b7dc7d7f455b9097549740daaa57f724d1025c5
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-11 18:16:25 +0200

    feat: added array examples

---
+++ FormatExceptionApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FormatExceptionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter an integer(0 for quit)");
+                    num = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"You entered the integer: {num}");
+                    if (num == 0) break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+        }
+    }
