d2cce3ca4f33488f786761d1ac4c170c8dd7d3ce
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-11 20:24:04 +0200

    feat: added stringbuilder logic

---
+++ ExtractCapitals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace ExtractCapitals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World!";
+            string upperS = ExtractCapitals(s);
+            Console.WriteLine(upperS);
+        }
+
+        public static string ExtractCapitals(string? s)
+        { 
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (char.IsUpper(c)) sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
