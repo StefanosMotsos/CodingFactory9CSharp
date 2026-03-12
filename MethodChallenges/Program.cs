75ea5faa38902ac072394d0c6f0ef4600572f6ca
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-12 20:33:54 +0200

    added: POCO class

--- MethodChallenges/Program.cs
+++ MethodChallenges/Program.cs
@@ -51,5 +51,90 @@ namespace MethodChallenges
             }
             return sb.ToString();
         }
+
+        // Creates a random pin of 4 digits. Random .next
+        public static string GeneratePin()
+        {
+            Random rand = new();
+
+            return rand.Next(1000, 10000).ToString();   // 1000 to 9999
+        }
+
+        // Filtering even numbers from an array
+        public static int[] FilterEven(int[] arr)
+        {
+            // return arr.Where(num => num % 2 == 0).ToArray();
+            return [.. arr.Where(num => num % 2 == 0)]; // spread > C# 12
+        }
+
+        // Bubble sort an array of ints
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // Swap arr[j] and arr[j + 1]
+                        (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]); // tuple swap
+                    }
+                }
+            }
+        }
+
+        // Binary search for an int in a sorted array
+        public static int BinarySearch(int[] arr, int val)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int med = (left + right) / 2;
+
+                if (arr[med] == val) return med; // found
+
+                if (arr[med] < val)
+                {
+                    left = med + 1; // search right half
+                }
+                else
+                {
+                    right = med - 1; // search left half
+                }
+            }
+
+            return -1;
+        }
+
+
+        // Check if two strings are anagrams
+        public static bool IsAnagram(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) return false;
+
+            char[] chars1 = str1.ToUpper().ToCharArray();
+            char[] chars2 = str2.ToUpper().ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            return new string(chars1) == new string(chars2);
+        }
+
+
+        // Calculate common elements between two arrays
+        public static int[] CommonElements(int[] arr1, int[] arr2)
+        {
+            HashSet<int> set1 = [.. arr1];
+            HashSet<int> set2 = [.. arr2];
+
+            set1.IntersectWith(set2);
+            return [.. set1];
+
+
+        }
     }
-}
