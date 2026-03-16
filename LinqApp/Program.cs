95267e1cd05945a504fb7aaef0557e9f48b0c47a
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-16 20:32:56 +0200

    feat: added collection sorting

---
+++ LinqApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LinqApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+            var allNumbers = from num in numbers select num;
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            var evenNumbers = from num in numbers
+                              where num % 2 == 0
+                              select num;
+        }
+    }
+}
