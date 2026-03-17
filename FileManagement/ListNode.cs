2f5602cdcd37eaf2cc6011a7101bb4c29609b766
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-17 19:43:47 +0200

    feat: added file reading app

---
+++ FileManagement/ListNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManagement
+{
+    internal class ListNode<T>
+    {
+        public T? Value { get; set; }
+
+        public int Count { get; set; }
+    }
+}
